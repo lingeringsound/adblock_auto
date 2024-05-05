624968fc27bfcc4f50faa0919e118f582a57fcac
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2024-05-05 21:50:13 +0800

    Add files via upload

--- until_function.sh
+++ until_function.sh
@@ -396,7 +396,7 @@ echo "${lite_content}" > "${file}"
 function lite_Uadblock_Rules(){
 local file="${1}"
 test ! -f "${file}" && return
-local lite_content="$(cat ${file} | grep -Ev '#\@\?#|\$\$|\$@\$|#\%#|#\@\%#|#\@\$\?#|#\$\?#|#\%#\/\/scriptlet|\$dnsrewrite=|\,replace=|:-abp-properties|:matches-attr|:matches-property|:nth-ancestor' | sort | uniq)"
+local lite_content="$(cat ${file} | grep -Ev '\$\$|\$@\$|#\%#|#\@\%#|#\@\$\?#|#\$\?#|#\%#\/\/scriptlet|\$dnsrewrite=|\,replace=|:-abp-properties|:matches-attr|:matches-property|:nth-ancestor' | sort | uniq)"
 echo "${lite_content}" > "${file}"
 }
 
