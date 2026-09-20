fefb31bd01ca7efe99284313dba1fa76e4e2974f
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2026-09-20 21:21:00 +0800

    Add files via upload

--- until_function.sh
+++ until_function.sh
@@ -643,55 +643,17 @@ busybox sed -i -E \
 }
 
 function count_filter_files() {
-local _cf_file _cf_n _cf_unit _cf_div _cf_int _cf_rem _cf_dec _cf_cunt_transfer
+local _cf_file _cf_n _cf_div _cf_s _cf_q _cf_r
 for _cf_file in "$@"
 do
-	[ -f "$_cf_file" ] || continue
-	[ -r "$_cf_file" ] || continue
-
-	# 过滤后开销太大 所以放弃过滤
-	#	_cf_n=$(grep -vc -e '^!' -e '^[[:space:]]*$' -e '^\[' -e '^#[^#]' "$_cf_file" 2>/dev/null)
-	#	_cf_n=$(grep -Evc '^!|^\[|^[[:space:]]*$|^#[^#]' "$_cf_file" 2>/dev/null)
-	#	_cf_n=$(sed '/^!/d;/^[[:space:]]*$/d;/^\[/d;/^#[^#]/d' "$_cf_file" 2>/dev/null | sed -n '$=')
-	#	_cf_n=$(sed -n '$=' "$_cf_file" 2>/dev/null)
+	[ -f "$_cf_file" ] && [ -r "$_cf_file" ] || continue
 	_cf_n=$(wc -l < "$_cf_file" 2>/dev/null)
-
-	case "$_cf_n" in ''|*[!0-9]*) continue ;; esac
-
-	[ "$_cf_n" = "0" ] && continue
-
-	if [ "$_cf_n" -ge "1000000" ]; then
-	    _cf_unit="m"
-	    _cf_div="1000000"
-	elif [ "$_cf_n" -ge "10000" ]; then
-	    _cf_unit="w"
-	    _cf_div="10000"
-	elif [ "$_cf_n" -ge "1000" ]; then
-	    _cf_unit="k"
-	    _cf_div="1000"
-	else
-	    _cf_unit=""
-	    _cf_div=1
-	fi
-
-	if [ -z "$_cf_unit" ]; then
-	    _cf_cunt_transfer="${_cf_n}"
-	else
-		_cf_int=$((_cf_n / _cf_div))
-		_cf_rem=$((_cf_n % _cf_div))
-	    if [ "$_cf_rem" -eq 0 ]; then
-			_cf_cunt_transfer="${_cf_int}${_cf_unit}"
-	    else
-			_cf_dec=$((_cf_rem * 10 / _cf_div))
-			_cf_cunt_transfer="${_cf_int}.${_cf_dec}${_cf_unit}"
-		fi
-	fi
-	if [ -n "${_cf_cunt_transfer}" ]; then
-		#echo -e "${_cf_file##*/}: ${_cf_cunt_transfer}"
-		echo "${_cf_cunt_transfer}"
-	else
-		echo "0"
-	fi
+	case "$_cf_n" in ''|*[!0-9]*|0) continue ;; esac
+	_cf_div="10000"; _cf_s="w"
+	[ "$_cf_n" -lt "10000" ] && { _cf_div="1000"; _cf_s="k"; }
+	[ "$_cf_n" -lt "1000" ] && { echo "$_cf_n"; continue; }
+	_cf_q=$((_cf_n / _cf_div)); _cf_r=$((_cf_n % _cf_div))
+	[ "$_cf_r" = "0" ] && echo "${_cf_q}${_cf_s}" || echo "${_cf_q}.$((_cf_r * 10 / _cf_div))${_cf_s}"
 done
 }
 
